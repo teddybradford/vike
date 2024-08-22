7b056f487a63daf6a23727518d554e0befbd0c82
brillout <github.com@brillout.com>
2024-08-22 09:32:58 +0000

    Deploying to gh-pages from @ vikejs/vike@33350db79fa116522b8ef4ab1e20beab49282865 🚀

--- assets/entries/pages_express.BHDjmdQ1.js
+++
@@ -1,1 +0,0 @@
-const e={onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}}};export{e as configValuesSerialized};
