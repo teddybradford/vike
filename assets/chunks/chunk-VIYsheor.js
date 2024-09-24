88556b3f3e651fa3dd256702fe685a6cb334e8fe
brillout <github.com@brillout.com>
2024-09-24 10:35:46 +0000

    Deploying to gh-pages from @ vikejs/vike@51f30f6611b69b41b4b047695ae6249516324440 🚀

--- assets/chunks/chunk-VIYsheor.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./chunk-BtLUXFqf.js";import{L as t}from"./chunk-ZlIoZ-pq.js";/* empty css              */function o(){return e.jsx("blockquote",{children:e.jsxs("p",{children:[e.jsx(t,{href:"/faq#i-can-t-achieve-what-i-want-can-i-get-help",children:"Feel free to reach out"})," if you run into an issue for integrating a tool."]})})}export{o as I};
