a0eaafb7d7c4fd149960e24181f44dd9c0a8affd
brillout <github.com@brillout.com>
2024-10-01 16:14:13 +0000

    Deploying to gh-pages from @ vikejs/vike@f8974c4f1a2926d3916582a84b724f01c3b56aed 🚀

---
+++ assets/entries/pages_apollo-graphql.CQOQckX2.js
@@ -0,0 +1,32 @@
+import{j as e,o as l,i as r}from"../chunks/chunk-B1niCXMT.js";import{L as o}from"../chunks/chunk-CFDfndwJ.js";/* empty css                      */import{C as s}from"../chunks/chunk-DNcm9zqy.js";import{D as d,a as p}from"../chunks/chunk-DDjtLoua.js";import{E as n}from"../chunks/chunk-D1t0_4f-.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-B0HWZO65.js";/* empty css                      */import"../chunks/chunk-iYPsLLPR.js";const c=[{pageSectionId:"initial-data",pageSectionLevel:2,pageSectionTitle:"Initial data"},{pageSectionId:"examples",pageSectionLevel:2,pageSectionTitle:"Examples"},{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function a(i){const t={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsxs(t.p,{children:["You can use Vike with ",e.jsx(t.a,{href:"https://www.apollographql.com",children:"Apollo GraphQL"}),"."]}),`
+`,e.jsx(t.p,{children:"This page documents how to manually integrate Apollo."}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Instead of manually integrating Apollo yourself, you can use:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-apollo",children:e.jsx(t.code,{children:"vike-react-apollo"})})}),`
+`]}),`
+`]}),`
+`,e.jsx(s,{children:e.jsxs(t.p,{children:["There isn't a ",e.jsx(o,{href:"/extensions",children:"Vike extension"})," for ",e.jsx(t.code,{children:"vike-vue"})," yet, but ",e.jsx(t.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"contributions welcome to create one"}),"."]})}),`
+`,e.jsx("h2",{id:"initial-data",children:"Initial data"}),`
+`,e.jsx(d,{toolType:"graphql",toolName:"Apollo"}),`
+`,e.jsx(t.p,{children:"Integration example:"}),`
+`,e.jsx(p,{toolName:"Apollo"}),`
+`,e.jsx(t.p,{children:"See also:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://www.apollographql.com/docs/react/performance/server-side-rendering/",children:"Apollo > Server-side rendering"})}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{href:"/integration#data-fetching"}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"examples",children:"Examples"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(n,{timestamp:"2024.07",repo:"brillout/vike-apollo-react"})," - With React"]}),`
+`,e.jsxs(t.li,{children:[e.jsx(n,{timestamp:"2024.07",repo:"brillout/vike-apollo-vue"})," - With Vue"]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://www.apollographql.com/docs/react/performance/server-side-rendering/",children:"Apollo > Server-side rendering"})}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{href:"/integration#data-fetching"}),`
+`]}),`
+`]})]})}function h(i={}){const{wrapper:t}=i.components||{};return t?e.jsx(t,{...i,children:e.jsx(a,{...i})}):a(i)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:h,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),D={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},NavHeader:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+NavHeader.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:r}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/apollo-graphql/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}}};export{D as configValuesSerialized};
