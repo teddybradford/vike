9f86a4086e314c52b5a206faaced2e3385647c0d
brillout <github.com@brillout.com>
2024-10-07 12:01:23 +0000

    Deploying to gh-pages from @ vikejs/vike@51ed8ef68ddd4890915621f0b93a51030e334fa2 🚀

--- assets/chunks/chunk-DKOJKFcH.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./chunk-CLKMuVP5.js";import{L as n}from"./chunk-ChGYZNgO.js";/* empty css              */function o(){return i.jsxs(i.Fragment,{children:["contradicts ",i.jsx(n,{href:"/lazy-transpiling",children:"Vite's lazy-transpiling approach"})," and it would, therefore, significantly slow down development speed"]})}export{o as V};
---
+++ assets/entries/pages_onAfterRenderClient.DxLf7f6z.js
@@ -0,0 +1,27 @@
+import{j as e,L as i,o as a,i as l}from"../chunks/chunk-BURd2QqB.js";import{L as n}from"../chunks/chunk-BvJCXD7Y.js";/* empty css                      */import{I as s}from"../chunks/chunk-BEtRqD3y.js";/* empty css                      *//* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-BKvOB9xx.js";/* empty css                      */import"../chunks/chunk-CCPW8xzl.js";/* empty css                      */const d=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(o){const t={code:"code",li:"li",p:"p",ul:"ul",...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"Environment: client."}),`
+`,e.jsx(s,{noCustomGuide:!0,children:e.jsx(t.code,{children:"onAfterRenderClient()"})}),`
+`,e.jsxs(t.p,{children:["Same as ",e.jsx(n,{href:"/onBeforeRenderClient",children:e.jsx(t.code,{children:"onBeforeRenderClient()"})})," but called at the end. It's usually used for integrating tools."]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/onBeforeRenderClient"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/onRenderClient"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/onHydrationEnd"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/onPageTransitionStart"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/onPageTransitionEnd"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/hooks"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/client"}),`
+`]}),`
+`]})]})}function p(o={}){const{wrapper:t}=o.components||{};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:p,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),P={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:i}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},NavHeader:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+NavHeader.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:l}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/onAfterRenderClient/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}}};export{P as configValuesSerialized};
