97907496749ef34853157d5135d0a18806b0943f
brillout <github.com@brillout.com>
2024-10-23 14:09:09 +0000

    Deploying to gh-pages from @ vikejs/vike@c1ddb61f6428f09a4323278eddea73b9bc216ce3 🚀

---
+++ assets/entries/pages_tanstack-query.KuVWm8B4.js
@@ -0,0 +1,37 @@
+import{j as e,L as s,o}from"../chunks/chunk-2vDHsprH.js";import{L as r}from"../chunks/chunk-C89OkL-F.js";/* empty css                      *//* empty css                      *//* empty css                      */import{U as l}from"../chunks/chunk-jcSGikmt.js";import{E as n}from"../chunks/chunk-DawB4P_P.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-u42NeO_H.js";/* empty css                      */const c=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function a(t){const i={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(i.p,{children:["You can use Vike with ",e.jsx(i.a,{href:"https://tanstack.com/query",children:"TanStack Query"}),"."]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsx(i.p,{children:"Instead of manually integrating TanStack Query yourself, you can use:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-query",children:e.jsx(i.code,{children:"vike-react-query"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-query",children:e.jsx(i.code,{children:"vike-vue-query"})})}),`
+`]}),`
+`]}),`
+`,e.jsx(i.p,{children:"To manually integrate TanStack Query, see:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(r,{href:"/integration#data-fetching"}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.p,{children:["Examples of integrating TanStack Query without using ",e.jsx(l,{name:!0}),":"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(n,{timestamp:"2024.04",repo:"Blankeos/hono-trpc"})," - Hono + tRPC + Solid + Tanstack Query + Nprogress"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(n,{timestamp:"2023.11",repo:"nabi-chan/vike-trpc-ssr"})," (using Vike)"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(n,{timestamp:"2023.11",repo:"phonzammi/vike-react-tanstack-query"})," (using Vike)"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(n,{timestamp:"2023.11",repo:"phonzammi/vike-react-query"})," (using ",e.jsx(r,{href:"/vike-react",children:e.jsx(i.code,{children:"vike-react"})}),")"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(n,{timestamp:"2023.07",repo:"alternativniy/vite-ssr-with-react-query"})," (using Vike)"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(n,{timestamp:"2023.04",repo:"YannBirba/vps-react-query"})," (using Vike)"]}),`
+`]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:["vite-plugin-ssr was the ",e.jsx(i.a,{href:"https://vite-plugin-ssr.com/vike",children:"previous name of Vike"}),"."]}),`
+`]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsx(i.p,{children:"You can als have a look at the source code of:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-query",children:e.jsx(i.code,{children:"vike-react-query"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-query",children:e.jsx(i.code,{children:"vike-vue-query"})})}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike/discussions/774",children:"Discussion about pre-rendering and TanStack Query"})}),`
+`]})]})}function d(t={}){const{wrapper:i}=t.components||{};return i?e.jsx(i,{...t,children:e.jsx(a,{...t})}):a(t)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:d,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),b={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:o}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/tanstack-query/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}}};export{b as configValuesSerialized};
---
+++ assets/entries/pages_vuex.D871NEdA.js
@@ -0,0 +1,15 @@
+import{j as e,L as n,o as s}from"../chunks/chunk-2vDHsprH.js";import{L as o}from"../chunks/chunk-C89OkL-F.js";/* empty css                      */import{C as a}from"../chunks/chunk-BlRpBXxk.js";/* empty css                      */import{E as l}from"../chunks/chunk-DawB4P_P.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-u42NeO_H.js";/* empty css                      *//* empty css                      */const d=[];function r(i){const t={a:"a",code:"code",li:"li",p:"p",ul:"ul",...i.components};return e.jsxs(e.Fragment,{children:[e.jsxs(t.p,{children:["You can use Vike with ",e.jsx(t.a,{href:"https://vuex.vuejs.org",children:"Vuex"}),"."]}),`
+`,e.jsx(a,{children:e.jsxs(t.p,{children:["There isn't a ",e.jsx(o,{href:"/extensions",children:"Vike extension"})," for Vuex yet, but ",e.jsx(t.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"contributions welcome to create one"}),"."]})}),`
+`,e.jsx(t.p,{children:"To manually integrate Vuex, see:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(o,{href:"/stores#ssr",doNotInferSectionTitle:!0}),`
+`]}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://vuejs.org/guide/scaling-up/ssr.html#cross-request-state-pollution",children:"Vue > SSR > Cross-Request State Pollution"})}),`
+`]}),`
+`,e.jsxs(t.p,{children:["Examples of integrating Vuex without using ",e.jsx(o,{href:"/vike-vue",children:e.jsx(t.code,{children:"vike-vue"})}),":"]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(l,{timestamp:"2023.10",repo:"brillout/vike-with-vuex"}),`
+`]}),`
+`]})]})}function u(i={}){const{wrapper:t}=i.components||{};return t?e.jsx(t,{...i,children:e.jsx(r,{...i})}):r(i)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:u,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),w={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:n}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/vuex/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}}};export{w as configValuesSerialized};
