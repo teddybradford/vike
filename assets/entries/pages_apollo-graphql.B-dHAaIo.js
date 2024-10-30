e28823078192d373a1816d9e7083cd34326c152b
brillout <github.com@brillout.com>
2024-10-30 15:02:25 +0000

    Deploying to gh-pages from @ vikejs/vike@41928ef4ca21ad16cbb0e3095d4e92fbd5b7385a 🚀

---
+++ assets/entries/pages_apollo-graphql.B-dHAaIo.js
@@ -0,0 +1,20 @@
+import{j as e,L as l,o as n,i as s}from"../chunks/chunk-B2Nsy3Ql.js";import{L as i}from"../chunks/chunk-raIxkx1Y.js";/* empty css                      */import{C as d}from"../chunks/chunk-DvyJGiE9.js";/* empty css                      */import{U as p}from"../chunks/chunk-Blu7WSIj.js";import{E as r}from"../chunks/chunk-Chd-08bW.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-u42NeO_H.js";/* empty css                      *//* empty css                      */const c=[];function a(o){const t={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...o.components};return e.jsxs(e.Fragment,{children:[e.jsxs(t.p,{children:["You can use Vike with ",e.jsx(t.a,{href:"https://www.apollographql.com",children:"Apollo GraphQL"}),"."]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["You can use ",e.jsx(t.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-apollo",children:e.jsx(t.code,{children:"vike-react-apollo"})})," instead of manually integrating Apollo yourself."]}),`
+`]}),`
+`,e.jsx(d,{children:e.jsxs(t.p,{children:["There isn't a ",e.jsx(i,{href:"/extensions",children:"Vike extension"})," for Apollo with ",e.jsx(t.code,{children:"vike-vue"})," yet, but ",e.jsx(t.a,{href:"https://github.com/vikejs/vike/issues/1715",children:"contributions welcome to create one"}),"."]})}),`
+`,e.jsx(t.p,{children:"To manually integrate Apollo, see:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(i,{href:"/integration#data-fetching"}),`
+`]}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://www.apollographql.com/docs/react/performance/server-side-rendering/",children:"Apollo > Server-side rendering"})}),`
+`]}),`
+`,e.jsxs(t.p,{children:["Examples of integrating Apollo without using ",e.jsx(p,{name:!0}),":"]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(r,{timestamp:"2024.07",repo:"brillout/vike-apollo-react"})," - With React"]}),`
+`,e.jsxs(t.li,{children:[e.jsx(r,{timestamp:"2024.07",repo:"brillout/vike-apollo-vue"})," - With Vue"]}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["You can also have a look at ",e.jsxs(t.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-apollo",children:["the source code of ",e.jsx(t.code,{children:"vike-react-apollo"})]}),"."]}),`
+`]})]})}function u(o={}){const{wrapper:t}=o.components||{};return t?e.jsx(t,{...o,children:e.jsx(a,{...o})}):a(o)}const h=Object.freeze(Object.defineProperty({__proto__:null,default:u,pageSectionsExport:c},Symbol.toStringTag,{value:"Module"})),z={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:l}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:n}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:s}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/apollo-graphql/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:h}}};export{z as configValuesSerialized};
