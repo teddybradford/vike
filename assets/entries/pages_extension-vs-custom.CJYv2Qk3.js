82701fdd0cc793cb1a41eeeb3ebf320ac7dff9b3
brillout <github.com@brillout.com>
2024-11-17 17:40:35 +0000

    Deploying to gh-pages from @ vikejs/vike@367b62bc02b12b68f98916bbcf23c8f8154a1029 🚀

---
+++ assets/entries/pages_extension-vs-custom.CJYv2Qk3.js
@@ -0,0 +1,34 @@
+import{j as e,L as a,o as s,i as l}from"../chunks/chunk-CBohOty8.js";import{L as n}from"../chunks/chunk-BozPSc_S.js";/* empty css                      *//* empty css                      *//* empty css                      */import{U as i}from"../chunks/chunk-Dtb6kgTu.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-UwW1iDb3.js";/* empty css                      *//* empty css                      */const d=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function r(o){const t={blockquote:"blockquote",li:"li",p:"p",ul:"ul",...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.p,{children:"With Vike, you have the choice between:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Using ",e.jsx(n,{href:"/extensions",children:"Vike extensions"}),", for a quick start."]}),`
+`,e.jsx(t.li,{children:"Implementing your own custom integrations, for full control over how tools are integrated."}),`
+`]}),`
+`,e.jsx(t.p,{children:"In general, we recommend always using Vike extensions, while falling back to custom integrations only if you have a clear reason."}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"Vike extensions are highly polished integrations. But they're also slightly opinionated and may not fit what you need."}),`
+`]}),`
+`,e.jsx(t.p,{children:"For example:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Start using Vike with ",e.jsx(i,{name:!0}),"."]}),`
+`,e.jsxs(t.li,{children:["If you run into a blocker because of ",e.jsx(i,{succinct:!0}),", then create an issue/discussion at the GitHub repository of ",e.jsx(i,{succinct:!0}),"."]}),`
+`,e.jsxs(t.li,{children:["If a maintainer of ",e.jsx(i,{succinct:!0})," replies that resolving your blocker is out-of-scope, then start considering a custom integration."]}),`
+`]}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsxs(t.p,{children:["You can also eject a Vike extension, for progressively moving from the ejected implementation of the Vike extension towards a custom integration. See ",e.jsx(n,{href:"/optional-control"}),"."]}),`
+`]}),`
+`,e.jsx(t.p,{children:"Large projects often need bespoke integrations, and opting out of Vike extensions can be a sensible choice. If that's your case, then you probably already have a very clear reason for wanting a custom integration."}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/optional-control"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/react#custom-integration"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/vue#custom-integration"}),`
+`]}),`
+`,e.jsxs(t.li,{children:[`
+`,e.jsx(n,{href:"/solid#custom-integration"}),`
+`]}),`
+`]})]})}function c(o={}){const{wrapper:t}=o.components||{};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}const u=Object.freeze(Object.defineProperty({__proto__:null,default:c,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),w={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:a}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:l}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/extension-vs-custom/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:u}}};export{w as configValuesSerialized};
---
+++ assets/entries/pages_tanstack-query.zWqEUXGg.js
@@ -0,0 +1,37 @@
+import{j as e,L as s,o,i as l}from"../chunks/chunk-CBohOty8.js";import{L as n}from"../chunks/chunk-BozPSc_S.js";/* empty css                      *//* empty css                      *//* empty css                      */import{U as c}from"../chunks/chunk-Dtb6kgTu.js";import{E as r}from"../chunks/chunk-BQzUqIiQ.js";/* empty css                      *//* empty css                      *//* empty css                      */import"../chunks/chunk-UwW1iDb3.js";/* empty css                      *//* empty css                      */const d=[{pageSectionId:"see-also",pageSectionLevel:2,pageSectionTitle:"See also"}];function a(t){const i={a:"a",blockquote:"blockquote",code:"code",li:"li",p:"p",ul:"ul",...t.components};return e.jsxs(e.Fragment,{children:[e.jsxs(i.p,{children:["You can use Vike with ",e.jsx(i.a,{href:"https://tanstack.com/query",children:"TanStack Query"}),"."]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsx(i.p,{children:"Instead of manually integrating TanStack Query yourself, you can use:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-query",children:e.jsx(i.code,{children:"vike-react-query"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-query",children:e.jsx(i.code,{children:"vike-vue-query"})})}),`
+`]}),`
+`]}),`
+`,e.jsx(i.p,{children:"To manually integrate TanStack Query, see:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[`
+`,e.jsx(n,{href:"/integration#data-fetching"}),`
+`]}),`
+`]}),`
+`,e.jsxs(i.p,{children:["Examples of integrating TanStack Query without using ",e.jsx(c,{name:!0}),":"]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(r,{timestamp:"2024.04",repo:"Blankeos/hono-trpc"})," - Hono + tRPC + Solid + Tanstack Query + Nprogress"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(r,{timestamp:"2023.11",repo:"nabi-chan/vike-trpc-ssr"})," (using Vike)"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(r,{timestamp:"2023.11",repo:"phonzammi/vike-react-tanstack-query"})," (using Vike)"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(r,{timestamp:"2023.11",repo:"phonzammi/vike-react-query"})," (using ",e.jsx(n,{href:"/vike-react",children:e.jsx(i.code,{children:"vike-react"})}),")"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(r,{timestamp:"2023.07",repo:"alternativniy/vite-ssr-with-react-query"})," (using Vike)"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(r,{timestamp:"2023.04",repo:"YannBirba/vps-react-query"})," (using Vike)"]}),`
+`]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsxs(i.p,{children:["vite-plugin-ssr was the ",e.jsx(i.a,{href:"https://vite-plugin-ssr.com/vike",children:"previous name of Vike"}),"."]}),`
+`]}),`
+`,e.jsxs(i.blockquote,{children:[`
+`,e.jsx(i.p,{children:"You can als have a look at the source code of:"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-react/tree/main/packages/vike-react-query",children:e.jsx(i.code,{children:"vike-react-query"})})}),`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike-vue/tree/main/packages/vike-vue-query",children:e.jsx(i.code,{children:"vike-vue-query"})})}),`
+`]}),`
+`]}),`
+`,e.jsx("h2",{id:"see-also",children:"See also"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsx(i.li,{children:e.jsx(i.a,{href:"https://github.com/vikejs/vike/discussions/774",children:"Discussion about pre-rendering and TanStack Query"})}),`
+`]})]})}function u(t={}){const{wrapper:i}=t.components||{};return i?e.jsx(i,{...t,children:e.jsx(a,{...t})}):a(t)}const p=Object.freeze(Object.defineProperty({__proto__:null,default:u,pageSectionsExport:d},Symbol.toStringTag,{value:"Module"})),w={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},Layout:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/Layout",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:s}},hydrationCanBeAborted:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/config",fileExportPathToShowToUser:["default","hydrationCanBeAborted"]},valueSerialized:{type:"js-serialized",value:!0}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"@brillout/docpress/renderer/onRenderClient",fileExportPathToShowToUser:[]},valueSerialized:{type:"pointer-import",value:o}},TopNavigation:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/+TopNavigation.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:l}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/tanstack-query/+Page.mdx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:p}}};export{w as configValuesSerialized};
