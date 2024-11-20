3fccae6b2d8eb14dd446f6cd2cae6c743324f3d3
brillout <github.com@brillout.com>
2024-11-20 20:32:51 +0000

    Deploying to gh-pages from @ vikejs/vike@ae3b7838f36489216cb2a626099b5248b5604f57 🚀

---
+++ assets/chunks/chunk-DJkEs-J9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./chunk-BJ4IHJDj.js";import{L as r}from"./chunk-BCA10iX_.js";/* empty css              *//* empty css              *//* empty css              */function n(){return e.jsxs(e.Fragment,{children:["From the perspective of the server, Vike is"," ",e.jsx(r,{text:"just a server middleware",href:"/integration#server-side-tools"}),"."]})}export{n as J};
