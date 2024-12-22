e30e5c3ada7e6f18f0816d9f42ffe3a4810ebea8
brillout <github.com@brillout.com>
2024-12-22 12:10:10 +0000

    Deploying to gh-pages from @ vikejs/vike@80951d3f9c2d6e437b6b3433d7ffc8dbd182a421 🚀

--- assets/entries/pages_pricing.BI7UStIc.js
+++ assets/entries/pages_pricing.BI7UStIc.js
@@ -49,8 +49,8 @@ import{j as e,a as t,L as k,o as S,i as v}from"../chunks/chunk-C75gEwxg.js";impo
 `,e.jsx("h3",{id:"do-sponsors-have-to-pay",children:"Do sponsors have to pay?"}),`
 `,e.jsx(n.p,{children:"No, Vike stays 100% gratis for companies sponsoring Vike (Bronze, Silver, Gold, and Platinum). Reach out and we will give you a free license key."}),`
 `,e.jsx("h3",{id:"is-vike-still-free-software",children:"Is Vike still Free Software?"}),`
-`,e.jsxs(n.p,{children:['Vike is still free as in "free speech" (not as in free beer). See the explanation ',e.jsx(n.a,{href:"https://www.gnu.org/philosophy/free-sw.en.html",children:"by the Free Software Foundation"})," and ",e.jsx(n.a,{href:"https://en.wikipedia.org/wiki/Free_software",children:"by Wikipedia"}),"."]}),`
-`,e.jsxs(n.p,{children:["We believe in Libre Software and that's we invented the Open Source Pricing: ",e.jsx(d,{doNotInferSectionTitle:!0,href:"#why-not-another-business-model",children:"other business models are fundamentally flawed"}),"."]}),`
+`,e.jsxs(n.p,{children:['Vike is still free as in "free speech" (not as in free beer), see explanations ',e.jsx(n.a,{href:"https://www.gnu.org/philosophy/free-sw.en.html",children:"by the Free Software Foundation"})," and ",e.jsx(n.a,{href:"https://en.wikipedia.org/wiki/Free_software",children:"by Wikipedia"}),"."]}),`
+`,e.jsxs(n.p,{children:["We believe in Libre Software, and we invented the Open Source Pricing because ",e.jsx(d,{doNotInferSectionTitle:!0,href:"#why-not-another-business-model",children:"other business models contradict Libre Software values"}),"."]}),`
 `,e.jsx(n.p,{children:"Code remains not only 100% open source but also 100% accessible: we believe money (and the lack thereof) should never be a showstopper for using code."}),`
 `,e.jsx("h3",{id:"why-not-another-business-model",children:"Why not another business model?"}),`
 `,e.jsx(n.p,{children:"We believe other business models are flawed in fundamental ways."}),`
