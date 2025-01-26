df3fd93fb760816bc7e389a064c319b1cd9b4241
brillout <github.com@brillout.com>
2025-01-26 21:47:00 +0000

    Deploying to gh-pages from @ vikejs/vike@df517f61471f8f4122e587c8e022009a307879d6 🚀

---
+++ assets/entries/entry-client-routing.B2lsIx7R.js
@@ -0,0 +1,1 @@
+import{i,a as n,b as e,g as a,r as s,m as r,c as o,s as l,d as c,e as g,f as u,h as d}from"../chunks/chunk-CclFcweH.js";async function S(){C();const t=P();i(),n(),await t}async function P(){e(a()===0),await s({scrollTarget:{preserveScroll:!0},isBackwardNavigation:null,isClientSideNavigation:!1})}function C(){r(),o(),l(),c(),g()}u();d();S();
